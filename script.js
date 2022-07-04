3552a2a5c00d396cebbea47c9c026e4d2652f6fb
reznd <gabriel999rv@gmail.com>
2022-07-04 16:43:40 -0300

    Iniciando tradução

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function() {
+    $(".flags-icon-us").click(function() {
+        //section
+        $(".title").text("Front End Developer & UX/UI Designer");
+        $(".experiencia-texto").text("Student and developer of Front End projects focused on UI/UX Design using HTML, CSS and JavaScript with Design prototypes using Figma.")
+
+    });
+});
